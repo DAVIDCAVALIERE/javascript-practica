8fc6a298a0526e440c1055946b18223d0f02b114
cavaliere <davincicavaliere@gmail.com>
2025-06-13 17:54:33 -0500

    dom

--- Intermediate/16-dom-exercises.js
+++ Intermediate/16-dom-exercises.js
@@ -4,8 +4,12 @@ Vídeo: https://youtu.be/iJvLAZ8MJ2E?t=23010
 */
 
 // 1. Crea un elemento (por ejemplo, un <h1 id="title">) y cambia su contenido a "¡Hola Mundo!"" al cargar la página
+const titulo = document.querySelector("#title");
+titulo.textContent = "Hola Mundo";
 
 // 2. Inserta una imagen con id="myImage" y cambia su atributo src a otra URL
+const imagen = document.querySelector("#myImage")
+imagen.setAttribute("src", "../Images/header.jpg")
 
 // 3. Crea un <div id="box"> sin clases y agrega la clase resaltado cuando se cargue la página
 
